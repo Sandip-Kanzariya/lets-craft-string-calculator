783f47cd31a7321bd3ff5d9758d813bb6180fc5c
Sandip-Kanzariya <105594748+Sandip-Kanzariya@users.noreply.github.com>
2024-07-30 22:09:01 +0530

    project overview in readme file

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Let's Craft String Calculator - Incubyte TDD 
 
+### Project Flow 
+
+![Project](https://github.com/user-attachments/assets/a04a803d-5a95-4cad-bc28-2d1665be27dd)
+
 ### Tech Stack : 
+- Test Driven Developement (TDD)
+- JAVA (Spring Boot)
+- Testing: JUnit 5
+
+### SetUp and Running: 
+1. Clone Repo.
+    ```bash
+    git clone https://github.com/Sandip-Kanzariya/lets-craft-string-calculator.git 
+    ```
+
+2. Run As Junit Test (Junit 5) 
+
--- src/main/java/project/calculator/CalculatorApplication.java
+++ src/main/java/project/calculator/CalculatorApplication.java
@@ -5,18 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "project.calculator.service")
 public class CalculatorApplication
 {
     public static void main( String[] args )
     {
     	SpringApplication.run(CalculatorApplication.class, args);
-        System.out.println( "String Calculator : " );
-        /* 
-	      	System.out.println("Enter Numbers : ");
-			Scanner scanner = new Scanner(System.in);
-			String numbers = scanner.nextLine();
-        */              
+        System.out.println( "String Calculator : " );              
     }
-    
 }
